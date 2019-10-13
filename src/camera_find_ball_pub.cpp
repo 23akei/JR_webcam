49a37b12b0bb466215351cc6fd2fc97590be1dd4
akei23 <tom0pr01fr.olem.001@gmail.com>
2019-10-13 21:29:45 +0900

    結構変更

--- src/camera_find_ball_pub.cpp
+++ src/camera_find_ball_pub.cpp
@@ -1,19 +1,45 @@
 //Find a ball and publish the data where it is.
+//パーティクルフィルタで探すモードと画像の画素数で探すモードを備える
+
+//画像処理のモード
+//0:なし
+//1:パーティクルフィルタ
+//2:画素数による検知
 
 #include <ros/ros.h>
 #include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Int32.h>
 #include <sstream>
 #include "Particle.hpp"
 
+//upper limit and lower limit of color
+#define B_MIN 96
+#define G_MIN 10
+#define R_MIN 10
+#define B_MAX 255
+#define G_MAX 127
+#define R_MAX 127
+
+int smode = 0;
+std_msgs::Int32 px;
+
+void getmode(std_msgs::Int32 status);
+
+std_msgs::Float64MultiArray particle(cv::Mat frame);
+std_msgs::Float64MultiArray mask(cv::Mat frame);
+
 
 int main(int argc, char **argv){
-
-    //ROS initialized
     ros::init(argc, argv, "point_publisher_node");
     ros::NodeHandle nh;
-    ros::Publisher ball_pub = nh.advertise<std_msgs::Float64MultiArray>("position_of_ball", 10);
+    ros::Publisher ball_pub = nh.advertise<std_msgs::Float64MultiArray>("position_of_ball", 10);    //検出したものの重心
+    ros::Publisher pixel_col = nh.advertise<std_msgs::Int32>("pixel", 10);  //特定色の画素数を出力
+    ros::Subscriber mode_sub = nh.subscribe<std_msgs::Int32>("search_mode", 10, getmode);   //モード変更
+
+    px.data = 0;
 
     //OpenCV initalized
+    cv::Mat img;
     cv::VideoCapture video;
     video.open(-1);     //id of camera (-1 is default)
 
@@ -21,66 +47,122 @@ int main(int argc, char **argv){
         std::cout << "can't open your video" << std::endl;
     }
 
-    bool start = false;
     std_msgs::Float64MultiArray point;
     point.data.resize(2);
 
-    ParticleFilter *pf = new ParticleFilter();
 
     ros::Rate looprate(10);
     while (ros::ok()){      //Main loop
-        cv::Mat frame;
-        video >> frame;
-
-        //終了判定の条件
-        if (frame.empty()){
-            break;
+        video >> img;
+        if(smode == 1 || smode == 0)
+            point = particle(img);
+        if(smode == 2){
+            point = mask(img);
+            pixel_col.publish(px);
         }
-
-        if (!start)
-        {
-            std::vector<int> upper = { frame.size().width, frame.size().height, 10, 10 };
-            std::vector<int> lower = { 0, 0, -10, -10 };
-            std::vector<int> noise = { 30, 30, 10, 10 };
-
-            pf = new ParticleFilter(300, frame.size().height, frame.size().width, upper, lower, noise);
-            start = true;
-        }
-
-        pf->Resampling();
-        pf->Predict();
-        pf->CalcWeight_b(frame);
-
-        Particle p = pf->Measure();
-        p.PrintParameter();
-
-        cv::Point pt = cv::Point(p.width, p.height);
-
-        //全部の点を表示
-        std::vector<Particle> particle = pf->GetPaticleVector();
-        for (int i = 0; i < particle.size(); ++i)
-        {
-            cv::Point pp = cv::Point(particle[i].width, particle[i].height);
-            cv::circle(frame, pp, 1, cv::Scalar(0, 255, 255), -1);
-        }
-
-        //中心を赤色で表示
-        cv::circle(frame, pt, 3, cv::Scalar(0, 0, 255), -1);
-
-        //publish the data
-        point.data[0] = pt.x;
-        point.data[1] = pt.y;
         ball_pub.publish(point);
         ros::spinOnce();
-
         looprate.sleep();
 
-        //for debug
-        cv::imshow("Camera", frame);
-        char c = (char)cv::waitKey(1);
-        if (c == 'q')
+        //終了判定の条件
+        if (img.empty()){
             break;
+        }       
     }
 
     return 0;
+}
+
+void getmode(std_msgs::Int32 status){
+    smode = status.data;
+}
+
+std_msgs::Float64MultiArray particle(cv::Mat frame){
+    bool start = false;
+    ParticleFilter *pf = new ParticleFilter();
+    std_msgs::Float64MultiArray point;
+    point.data.resize(2);
+    if (!start)
+    {
+        std::vector<int> upper = { frame.size().width, frame.size().height, 10, 10 };
+        std::vector<int> lower = { 0, 0, -10, -10 };
+        std::vector<int> noise = { 30, 30, 10, 10 };
+
+        pf = new ParticleFilter(300, frame.size().height, frame.size().width, upper, lower, noise);
+        start = true;
+    }
+
+    pf->Resampling();
+    pf->Predict();
+    pf->CalcWeight_b(frame);
+
+    Particle p = pf->Measure();
+    p.PrintParameter();
+
+    cv::Point pt = cv::Point(p.width, p.height);
+
+    //全部の点を表示
+    std::vector<Particle> particle = pf->GetPaticleVector();
+    for (int i = 0; i < particle.size(); ++i)
+    {
+        cv::Point pp = cv::Point(particle[i].width, particle[i].height);
+        cv::circle(frame, pp, 1, cv::Scalar(0, 255, 255), -1);
+    }
+
+    //中心を赤色で表示
+    cv::circle(frame, pt, 3, cv::Scalar(0, 0, 255), -1);
+
+    //publish the data
+    point.data[0] = pt.x;
+    point.data[1] = pt.y;
+
+    //for debug
+    /*cv::imshow("Camera", frame);
+    char c = (char)cv::waitKey(1);
+    if (c == 'q')
+        break;*/
+
+    return point;
+}
+
+std_msgs::Float64MultiArray mask(cv::Mat img_in){
+    px.data = 0;
+    std_msgs::Float64MultiArray pt;
+    pt.data.resize(2);
+
+    cv::Scalar s_min = cv::Scalar(B_MIN, G_MIN, R_MIN);
+    cv::Scalar s_max = cv::Scalar(B_MAX, G_MAX, R_MAX);
+    
+    cv::Point center;
+    cv::Mat img_mask, img_pick, img_gray, img_gauss;
+    
+    //Pick up
+    cv::inRange(img_in, s_min, s_max, img_mask);
+    img_in.copyTo(img_pick, img_mask);
+
+    //二値化　平滑化
+    cv::GaussianBlur(img_pick, img_gauss, cv::Size(5, 5), 2, 5);
+    cv::cvtColor(img_gauss, img_gray, CV_BGR2GRAY, 0);
+    cv::adaptiveThreshold(img_gray, img_gray, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 3, 2);
+
+     //特定色のピクセルの検出
+    //int np = 0;
+    for(int y = 0; y < img_gray.rows; y++){
+        for(int x = 0; x < img_gray.cols; x++){
+            if(img_gray.at<cv::Vec3b>(y, x)[0] == 0 && 
+            img_gray.at<cv::Vec3b>(y, x)[1] == 0 &&
+            img_gray.at<cv::Vec3b>(y, x)[2] == 0){
+                center.x += x;
+                center.y += y;
+                px.data++;
+            }
+        }
+    }
+
+    //for debug
+    //cv::imshow("win", img_pick);
+
+    pt.data[0] = center.x / px.data;
+    pt.data[1] = center.y / px.data;
+    return pt;
 }
--- src/tracking_ball.cpp
+++ src/tracking_ball.cpp
@@ -1,4 +1,8 @@
 //subuscribe the point of the ball and tracking it
+//ボールを見失った時の挙動を指示する
+//ボールを見失ったという判定はパーティクルフィルタで行う
+//最初は後退しながら目的の画素で探し、発見後パーティクルフィルタで追跡
+//画像処理はposition_of_ballトピックから入手
 
 #include <ros/ros.h>
 #include <std_msgs/Float64MultiArray.h>
@@ -6,48 +10,68 @@
 #include <geometry_msgs/Twist.h>
 
 #define SPD (1.0)
-#define FRAME_WIDTH (800)
+#define FRAME_WIDTH (640)
+#define FRAME_HEIGHT (480)
 #define FRAME_CENTER (100)  //中央とみなす範囲。中心からプラスマイナスこれだけ。
 
+int margin = 20;    //端の検出の余裕
+std_msgs::Float64MultiArray pos;
+
 //購読時コールバック関数
-void slide(std_msgs::Float64MultiArray position);
-void track(std_msgs::Int32 touch);
+void repos(std_msgs::Float64MultiArray position);
+void numpx(std_msgs::Int32 np);
 
 int status = 0;
 
 ros::NodeHandle nh;
-ros::Subscriber get_position = nh.subscribe<std_msgs::Float64MultiArray>("position_of_ball", 10, slide);
-ros::Subscriber touch_sensor = nh.subscribe<std_msgs::Int32>("touch_sensor", 10, track);
+ros::Publisher img_mode = nh.advertise<std_msgs::Int32>("search_mode", 1); //画像処理のモードを指定
+ros::Subscriber get_position = nh.subscribe<std_msgs::Float64MultiArray>("position_of_ball", 10, repos);
+ros::Subscriber get_numpx = nh.subscribe<std_msgs::Int32>("pixel", 10, numpx);  //ピクセル数
 ros::Publisher tracking = nh.advertise<geometry_msgs::Twist>("sub", 1);
 
 
 int main(int argc, char **argv){
+    geometry_msgs::Twist twist;
+    std_msgs::Int32 sts;
+    sts.data = 0;
+    pos.data.resize(2);
+
     ros::init(argc, argv, "tracking_ball_node");
-    
-    ros::spin();
+    ros::Rate lpl(10);
+    while(ros::ok()){
+        if(status == 1){    //後退しつつボール探索
+            twist.linear.y = -SPD;
+            tracking.publish(twist);
+        }
+        if(status == 2){    //前進しながら追跡
+            twist.linear.y = SPD/2;
+
+            //重心の位置によって左右を制御
+            if(pos.data[0] > FRAME_CENTER + FRAME_WIDTH/2 && pos.data[0] < FRAME_WIDTH - margin){
+                twist.linear.x = SPD;
+            }
+            if(pos.data[0] < FRAME_WIDTH/2 - FRAME_CENTER && pos.data[0] > margin){
+                twist.linear.y - -SPD;
+            }
+            //中央にボールが来た場合、通常制御に戻す
+            if(pos.data[0] > FRAME_WIDTH/2 - FRAME_CENTER && pos.data[0] < FRAME_WIDTH/2 + FRAME_CENTER){
+                status = 0;
+            }
+        }
+
+        sts.data = status;
+        img_mode.publish(sts);
+        ros::spinOnce();
+    }
     
     return 0;
 }
 
-void slide(std_msgs::Float64MultiArray position){
-    if(status == 1){
-        geometry_msgs::Twist twist;
-        if(position.data[0] < FRAME_WIDTH/2 - FRAME_CENTER){//left
-            twist.linear.x = -SPD;
-            tracking.publish(twist);
-        }else if(position.data[0] > FRAME_WIDTH/2 + FRAME_CENTER){//right
-            twist.linear.x = SPD;
-            tracking.publish(twist);
-        }else{//center
-            twist.linear.y = SPD;
-            tracking.publish(twist);
-        }
-    }
-    ros::spin();
+void repos(std_msgs::Float64MultiArray position){
+    pos = position;
 }
 
-void track(std_msgs::Int32 touch){
-    if(touch.data == 0) status = 1;
-    else if(touch.data == 1) status = 0;
+void numpx(std_msgs::Int32 np){
+    if(np.data > 3000) status = 1;
+    if(np.data < 3000) status = 2;
 }
-
