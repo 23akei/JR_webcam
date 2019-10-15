8c9780ad5d78d753be3d7e6dcb540fe4c92793e1
Akei Kanzaki <50440089+akei23@users.noreply.github.com>
2019-10-15 17:19:57 +0900

    Add comments and some changes

--- src/tracking_ball.cpp
+++ src/tracking_ball.cpp
@@ -10,9 +10,9 @@
 #include <geometry_msgs/Twist.h>
 
 #define SPD (1.0)
-#define SIKII (3000)
-#define FRAME_WIDTH (640)
-#define FRAME_HEIGHT (480)
+#define SIKII (3000)    //抽出ピクセル数のしきい値
+#define FRAME_WIDTH (640)   //フレームの横のピクセル数
+#define FRAME_HEIGHT (480)  //フレームの縦のピクセル数
 #define FRAME_CENTER (100)  //中央とみなす範囲。中心からプラスマイナスこれだけ。
 
 int margin = 20;    //端の検出の余裕
@@ -26,12 +26,13 @@ int status = 0;
 
 ros::NodeHandle nh;
 ros::Publisher img_mode = nh.advertise<std_msgs::Int32>("search_mode", 1); //画像処理のモードを指定
-ros::Subscriber get_position = nh.subscribe<std_msgs::Float64MultiArray>("position_of_ball", 10, repos);
+ros::Subscriber get_position = nh.subscribe<std_msgs::Float64MultiArray>("position_of_ball", 10, repos);    //ボールの位置（重心）
 ros::Subscriber get_numpx = nh.subscribe<std_msgs::Int32>("pixel", 10, numpx);  //ピクセル数
-ros::Publisher tracking = nh.advertise<geometry_msgs::Twist>("sub", 1);
+ros::Publisher tracking = nh.advertise<geometry_msgs::Twist>("sub", 1); //動作指示
 
 
 int main(int argc, char **argv){
+    //initialization
     geometry_msgs::Twist twist;
     std_msgs::Int32 sts;
     sts.data = 0;
@@ -39,6 +40,8 @@ int main(int argc, char **argv){
 
     ros::init(argc, argv, "tracking_ball_node");
     ros::Rate lpl(10);
+    
+    //Main Loop
     while(ros::ok()){
         if(status == 1){    //後退しつつボール探索
             twist.linear.y = -SPD;
@@ -63,16 +66,17 @@ int main(int argc, char **argv){
         sts.data = status;
         img_mode.publish(sts);
         ros::spinOnce();
+        lpl.sleep();
     }
     
     return 0;
 }
 
-void repos(std_msgs::Float64MultiArray position){
+void repos(std_msgs::Float64MultiArray position){   //重心の更新
     pos = position;
 }
 
-void numpx(std_msgs::Int32 np){
+void numpx(std_msgs::Int32 np){ //抽出ピクセル数の更新に応じて動作ステータスを変更
     if(np.data > SIKII) status = 1;
     if(np.data < SIKII) status = 2;
 }
